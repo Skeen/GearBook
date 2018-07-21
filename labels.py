075aff6278e71b844bbebbe0d106f8a0315b5d10
Emil Madsen <emil@magenta.dk>
2018-07-22 00:08:01 +0200

    Updated python code

--- labels.py
+++ labels.py
@@ -1,62 +1,124 @@
 #!/usr/bin/env python
 import os
+import math
 from fpdf import FPDF
+from PIL import Image
 import qrcode
 
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font('Arial', 'B', 16)
+# Constants
+mm2pt = 72/25.4
+logo_filename = 'logo.png'
 
-columns = 5
-rows = 13
-
+# Dimensions of our paper (A4)
 paper_height = 297
 paper_width = 210
 
-margin_top = 10
-margin_bottom = 10
-margin_left = 10
-margin_right = 10
+# Information about labels
+# Number of columns / rows
+columns = 5
+rows = 13
+# Size of each label / cell
+cell_height = 21.2
+cell_width = 38
 
-cell_height = float(paper_height - margin_top - margin_bottom) / rows
-cell_width = float(paper_width - margin_left - margin_right) / columns
+## Number of columns / rows
+#columns = 3
+#rows = 8
+## Size of each label / cell
+#cell_height = 36
+#cell_width = 70
 
-#label_width = 38
-#label_height = 21.2
-#
-#cell_width_spacing = (cell_width - label_width) / columns
-#cell_height_spacing = (cell_height - label_height) / columns
-#
-#print cell_width_spacing
-#print cell_height_spacing
+# Start ID
+start_id = 1
+# Maximum number of digits in ID
+max_digits = 4
+# max_digits = int(math.log10(start_id+(columns*rows)))+1
 
-start_id = 0
+## Information about margins
+# Calculate our total margins
+margins_height = float(paper_height - (cell_height * rows))
+margins_width = float(paper_width - (cell_width * columns))
+# Calculate our margins (assumes centered)
+margin_left = margins_width / 2
+margin_right = margins_width / 2
+margin_top = margins_height / 2
+margin_bottom = margins_height / 2
+
+# Find smallest dimension (for QR code max-size)
+max_dimension = min(cell_height, cell_width)
+
+# Set our border size (used to seperate things)
+image_border = max_dimension/10
+
+# Open our PDF document
+pdf = FPDF()
+pdf.set_margins(left=margin_left, top=margin_top)
+pdf.add_page()
+
+# Figure out our font-size
+# We need to know the format of our logo
+logo_ratio = None
+with Image.open(logo_filename) as img:
+    width, height = img.size
+    logo_ratio = float(height)/width
+# Calculate the logo size
+logo_width = cell_width-max_dimension-image_border
+logo_height = logo_width*logo_ratio
+# Calculate how much space will be left under the logo
+height_left = cell_height - logo_height - image_border
+width = None
+# Start with pt=0, and go to our max height, trying to maximize width
+pdf.set_font('Arial', '', 0)
+for size in range(int(height_left*mm2pt)):
+    width = pdf.get_string_width("x" * max_digits)
+    # Stop if we get too wide
+    if width > logo_width:
+        break
+    pdf.set_font('Arial', '', size)
+
 image_folder = 'img/'
 if not os.path.exists(image_folder):
     os.makedirs(image_folder)
 
-current_y = 0
-current_x = 0
+pdf.set_y(margin_top)
 for row in range(rows):
-    current_x = 0
+    pdf.set_x(margin_left)
+    print "new row"
     for column in range(columns):
         cell_id = start_id + (column + row*columns)
-        qr_filename = image_folder + str(start_id + columns*row) + ".png"
+        qr_filename = image_folder + str(cell_id) + ".png"
         url = 'http://ralph.atlas.magenta.dk/item/' + str(cell_id)
         # Generate QR CODE
-        img = qrcode.make(url)
+        img = qrcode.make(url, border=0)
         img.save(qr_filename, "PNG")
-        # Find smallest dimension (for QR code max-size)
-        max_dimension = min(cell_height, cell_width)
         # Write in our QR code
         pdf.image(
-            qr_filename, margin_left+current_x, margin_top+current_y,
-            max_dimension, max_dimension, link=url
+            x=pdf.get_x()+image_border, 
+            y=pdf.get_y()+image_border,
+            w=max_dimension-image_border*2, 
+            h=max_dimension-image_border*2, 
+            name=qr_filename, 
+            link=url,
         )
-        pdf.text(margin_left+current_x+max_dimension, margin_top+current_y+cell_height, str(row) + ':' + str(column) + " (" + str(cell_id) + ")")
-        pdf.rect(margin_left+current_x, margin_top+current_y, cell_width, cell_height)
-
-        current_x += cell_width
-    current_y += cell_height
+        # Add the logo
+        pdf.image(
+            x=pdf.get_x()+max_dimension, 
+            y=pdf.get_y()+image_border,
+            w=logo_width,
+            h=logo_height,
+            name=logo_filename,
+        )
+        # Print the id
+        pdf.text(
+            # Place centered under the logo (only center with max_digits)
+            x=pdf.get_x()+max_dimension+logo_width/2-width/2,
+            # Place on the bottom of the cell minus one border
+            y=pdf.get_y()+cell_height-image_border,
+            txt=str(cell_id)
+        )
+        pdf.rect(x=pdf.get_x(), y=pdf.get_y(), w=cell_width, h=cell_height)
+        pdf.set_x(pdf.get_x() + cell_width)
+        
+    pdf.set_y(pdf.get_y() + cell_height)
 
 pdf.output('tuto1.pdf', 'F')
